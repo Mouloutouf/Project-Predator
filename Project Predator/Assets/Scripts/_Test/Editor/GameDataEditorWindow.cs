95283012044b51fb025c0b9378e832fb1b2c6427
Maxence <maxdurlon@gmail.com>
2020-10-26 10:05:34 +0100

    Reset Project

---
+++ Project Predator/Assets/Scripts/_Test/Editor/GameDataEditorWindow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GameDataEditorWindow : EditorWindow
+{
+    public static void Open(GameDataObject dataObject)
+    {
+        
+    }
+}
